566733c9a2b0bce11fa50c4060f9247517910f6e
daviddki <daviddkim0@gmail.com>
2025-05-07 16:03:32 -0700

    Update fingers.cpp

--- hand/fingers.cpp
+++ hand/fingers.cpp
@@ -1,19 +1,38 @@
 #include "fingers.hpp"
 
-AccelStepper finger1(InterfaceType, X_STEP_PIN, X_DIR_PIN);
-AccelStepper finger2(InterfaceType, Y_STEP_PIN, Y_DIR_PIN);
-AccelStepper finger3(InterfaceType, Z_STEP_PIN, Z_DIR_PIN);
 
-void setup() {
+AccelStepper motor0(InterfaceType, X_STEP_PIN, X_DIR_PIN);
+AccelStepper motor1(InterfaceType, Y_STEP_PIN, Y_DIR_PIN);
+AccelStepper motor2(InterfaceType, Z_STEP_PIN, Z_DIR_PIN);
+
+Finger finger0(motor0, 1);
+Finger finger1(motor1, 1);
+Finger finger2(motor2, 1);
+
+void motor_setup() {
   //400 and 200 are temporary values
   //we will have to play around with the hardware a bit
   //before the desired speed/acceleration is found
-  finger1.setMaxSpeed(400);
-  finger2.setAcceleration(200);
+  motor0.setMaxSpeed(400);
+  motor0.setAcceleration(200);
 
-  finger2.setMaxSpeed(400);
-  finger2.setAcceleration(200);
+  motor1.setMaxSpeed(400);
+  motor1.setAcceleration(200);
 
-  finger3.setMaxSpeed(400);
-  finger3.setAcceleration(200);
+  motor2.setMaxSpeed(400);
+  motor2.setAcceleration(200);
 }
+
+void open_finger(Finger& finger) {
+  if (!finger->open) {
+    finger->motor.moveTo(fingerSteps);//check polarity. make sure positive value opens finger
+    finger->open = 1;
+  }
+}
+void close_finger(Finger& finger) {
+  if (finger->open) {
+    finger->motor.moveTo(-fingerSteps);//check polarity
+    finger->open = 0;
+  }
+}
+
