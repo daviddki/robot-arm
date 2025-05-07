d6cfd2473d1378816632754b0981d7dda770d79e
daviddki <daviddkim0@gmail.com>
2025-05-07 01:45:43 -0700

    Create fingers.hpp

---
+++ hand/fingers.hpp
@@ -0,0 +1,17 @@
+#ifndef _FINGERS_HPP
+#define _FINGERS_HPP
+
+#include <AccelStepper.h>
+
+#define InterfaceType AccelStepper::DRIVER
+
+#define X_STEP_PIN 54
+#define X_DIR_PIN 55
+
+#define Y_STEP_PIN 60
+#define Y_DIR_PIN 61
+
+#define Z_STEP_PIN 46
+#define Z_DIR_PIN 48
+
+#endif /* _FINGERS_HPP */
