19ba6e0c580353b4450d0e131b4a5a573b88ae4a
daviddki <daviddkim0@gmail.com>
2025-05-07 16:01:24 -0700

    Update main.ino

--- arm/main.ino
+++ arm/main.ino
@@ -1,7 +1,30 @@
 #include "main.hpp"
 #include "motor.hpp"
 
+void setup() {
+  pinMode(RPS_INPUT, INPUT);
+  pinMode(RPS_OUTPUT, OUTPUT);
+}
+
+void rps() {
+  for (int i = 0; i < 3; ++i) {
+    //arm up
+    if (i == 2) {
+      digitalWrite(RPS_OUTPUT, HIGH);
+      delay(10);
+      digitalWrite(RPS_OUTPUT, LOW);
+    }
+    //arm down
+  }
+}
+
 int main() {
+  motor_setup();
   setup();
-  
+  while (1) {
+    if (RPS_INPUT == 0) {
+      rps();
+    }
+  }
+  return 0;
 }
