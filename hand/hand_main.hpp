ca7dfb50f51f971ffea40898309ef6dbfe589cff
daviddki <daviddkim0@gmail.com>
2025-05-07 16:03:14 -0700

    Create hand_main.hpp

---
+++ hand/hand_main.hpp
@@ -0,0 +1,16 @@
+#ifndef _HAND_MAIN_HPP
+#define _HAND_MAIN_HPP
+
+#include <stdlib.h>
+
+#define RPS_INPUT 2; //placeholder
+
+void setup();
+
+void rock();
+void paper();
+void scissors();
+
+void rps();
+
+#endif /* _MAIN_HPP */
