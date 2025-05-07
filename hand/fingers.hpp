168cfbba700bcb26feec5d483678819f687ea8f9
daviddki <daviddkim0@gmail.com>
2025-05-07 16:03:56 -0700

    Update fingers.hpp

--- hand/fingers.hpp
+++ hand/fingers.hpp
@@ -14,4 +14,24 @@
 #define Z_STEP_PIN 46
 #define Z_DIR_PIN 48
 
+#define fingerSteps 500 //placeholder
+
+typedef struct {
+  AccelStepper motor;
+  bool open;
+} Finger;
+
+void motor_setup();
+
+void open_finger(Finger& finger);
+void close_finger(Finger& finger);
+
+extern AccelStepper motor0;
+extern AccelStepper motor1;
+extern AccelStepper motor2;
+
+extern Finger finger0;
+extern Finger finger1;
+extern Finger finger2;
+
 #endif /* _FINGERS_HPP */
