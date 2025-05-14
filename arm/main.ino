fb28236905e66bf685ba8f4ab856b3fb194e8b7d
daviddki <daviddkim0@gmail.com>
2025-05-14 12:22:57 -0700

    Update main.ino

--- arm/main.ino
+++ arm/main.ino
@@ -1,24 +1,33 @@
 #include "main.hpp"
-#include "motor.hpp"
+#include "stepper.hpp"
+#include "servo.hpp"
 
 void setup() {
-  pinMode(RPS_INPUT, INPUT);
+  /*pinMode(RPS_INPUT, INPUT);
   pinMode(RPS_OUTPUT, OUTPUT);
-  motor_setup();
-
-  elbowMotor.moveTo(2000);
+  stepper_setup();*/
+  Serial.begin(9600);
+  servoSetup();
+  int pos;
+  for (pos = 0; pos <= 180; pos++) {
+    thumbDown.write(pos);
+    delay(15);
+  }
+  for (pos = 180; pos >= 0; pos--) {
+    thumbDown.write(pos);
+    delay(15);
+  }
 }
 
 void loop() {
   /*
-  if (!digitalRead(RPS_INPUT)) {
-    rps();
+  if (digitalRead(RPS_INPUT)) {
+    //rps();
+    Serial.println("hi");
+    elbowMotor.moveTo(2000);
   }
-  */
-  
   elbowMotor.run();
-
-
+  */
 }
 
 void rps() {
