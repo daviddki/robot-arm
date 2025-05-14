4da1f198ce9e2d67cc3b70f040ae3f2676c1de5f
daviddki <daviddkim0@gmail.com>
2025-05-14 00:25:03 -0700

    Update main.ino

--- arm/main.ino
+++ arm/main.ino
@@ -2,16 +2,11 @@
 #include "motor.hpp"
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("0");
-  Serial.println(elbowMotor.currentPosition());
   pinMode(RPS_INPUT, INPUT);
   pinMode(RPS_OUTPUT, OUTPUT);
-  Serial.println("1");
   motor_setup();
-  Serial.println("2");
 
-  elbowMotor.moveTo(1000);
+  elbowMotor.moveTo(2000);
 }
 
 void loop() {
@@ -20,10 +15,9 @@ void loop() {
     rps();
   }
   */
-  if (elbowMotor.distanceToGo() != 0) {
-    Serial.println(elbowMotor.distanceToGo());
-    elbowMotor.run();
-  }
+  
+  elbowMotor.run();
+
 
 }
 
