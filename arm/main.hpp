01ea7f1654adb6d31241ab5c7124b558357f6348
daviddki <daviddkim0@gmail.com>
2025-05-14 12:23:23 -0700

    Update main.hpp

--- arm/main.hpp
+++ arm/main.hpp
@@ -1,7 +1,7 @@
 #ifndef _MAIN_HPP
 #define _MAIN_HPP
 
-#define RPS_INPUT 1 //placeholder. can change later
+#define RPS_INPUT 34 //placeholder. can change later
 #define RPS_OUTPUT 2 //placeholder
 
 void setup();
