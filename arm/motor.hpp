bdd43cfd28ac3b315939a9e4fe72e6174e51ba5e
daviddki <daviddkim0@gmail.com>
2025-05-13 23:42:58 -0700

    Update all pin numbers

--- arm/motor.hpp
+++ arm/motor.hpp
@@ -6,20 +6,31 @@
 #define InterfaceType AccelStepper::DRIVER
 
 //Define pins for stepper motors
-#define X_STEP_PIN 54
-#define X_DIR_PIN 55
+#define X_STEP_PIN 54 //A0
+#define X_DIR_PIN 55 //A1
+#define X_EN_PIN 38
 
-#define Y_STEP_PIN 60
-#define Y_DIR_PIN 61
+#define Y_STEP_PIN 60 //A6
+#define Y_DIR_PIN 61 //A7
+#define Y_EN_PIN 56 //A2
 
 #define Z_STEP_PIN 46
 #define Z_DIR_PIN 48
+#define Z_EN_PIN 62 //A8
 
 #define E0_STEP_PIN 26
 #define E0_DIR_PIN 28
+#define E0_EN_PIN 24
 
 #define E1_STEP_PIN 36
 #define E1_DIR_PIN 34
+#define E1_EN_PIN 30
+
+extern AccelStepper baseMotor;
+extern AccelStepper leftMotor;
+extern AccelStepper rightMotor;
+extern AccelStepper elbowMotor;
+extern AccelStepper wristMotor;
 
 void motor_setup();
 
