7cd10a99b7c9d4d6a7a89ee04e18a22105095a4d
daviddki <daviddkim0@gmail.com>
2025-05-13 23:43:26 -0700

    Update motor setup

--- arm/motor.cpp
+++ arm/motor.cpp
@@ -11,7 +11,7 @@ AccelStepper rightMotor(InterfaceType, Z_STEP_PIN, Z_DIR_PIN);
 AccelStepper elbowMotor(InterfaceType, E0_STEP_PIN, E0_DIR_PIN);
 
 //For the motor that moves the wrist joint
-AccelStepper wristMotor(InterfaceType, E1_STEP_PIN, E0_DIR_PIN);
+AccelStepper wristMotor(InterfaceType, E1_STEP_PIN, E1_DIR_PIN);
 
 void motor_setup() {
   //400 and 200 are temporary values
@@ -19,16 +19,31 @@ void motor_setup() {
   //before the desired speed/acceleration is found
   baseMotor.setMaxSpeed(400);
   baseMotor.setAcceleration(200);
+  baseMotor.setEnablePin(X_EN_PIN);
+  baseMotor.setPinsInverted(false, false, true);
+  baseMotor.enableOutputs();
 
   leftMotor.setMaxSpeed(400);
   leftMotor.setAcceleration(200);
+  leftMotor.setEnablePin(Y_EN_PIN);
+  leftMotor.setPinsInverted(false, false, true);
+  leftMotor.enableOutputs();
 
   rightMotor.setMaxSpeed(400);
   rightMotor.setAcceleration(200);
+  rightMotor.setEnablePin(Z_EN_PIN);
+  rightMotor.setPinsInverted(false, false, true);
+  rightMotor.enableOutputs();
 
   elbowMotor.setMaxSpeed(400);
   elbowMotor.setAcceleration(200);
+  elbowMotor.setEnablePin(E0_EN_PIN);
+  elbowMotor.setPinsInverted(false, false, true);
+  elbowMotor.enableOutputs();
 
   wristMotor.setMaxSpeed(400);
   wristMotor.setAcceleration(200);
+  wristMotor.setEnablePin(E1_EN_PIN);
+  wristMotor.setPinsInverted(false, false, true);
+  wristMotor.enableOutputs();
 }
